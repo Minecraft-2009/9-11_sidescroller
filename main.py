d87b698ca844df59b84b9a43e29b94cb1d08c64e
GrinchBob43 <155773877+GrinchBob43@users.noreply.github.com>
2024-04-12 01:10:18 +0000

    removed sprite backgrounds

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import pygame
 pygame.init()
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 500
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 1, 1)
+stage_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 0)
 clock = pygame.time.Clock()
 running = True
 
 velocity = 5
 
-player = pygame.image.load("images/b-767.png")
+player = pygame.image.load("images_new/plane.png")
 player_rect = player.get_rect()
 player_rect.centerx = 150
 player_rect.centery = 300
+
+
+
+# background_rect = background.get_rect()
+# background_rect.centerx = 500
+# background_rect.centery = 250
 
 # tow = pygame.image.load("images/twin_towers.png")
 # towrect = tow.get_rect()
@@ -31,7 +38,7 @@ def player_move(self):
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, x, y, typ3): 
         super().__init__() 
-        self.image = pygame.image.load(f"images/{typ3}.png")
+        self.image = pygame.image.load(f"images_new/{typ3}.png")
         self.rect = self.image.get_rect()
 
         self.x = x
@@ -46,15 +53,32 @@ class Obstacle(pygame.sprite.Sprite):
         self.x -= 1
         self.rect.bottomleft = (self.x, self.y)
 
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, x, y,): 
+        super().__init__() 
+        self.image = pygame.image.load(f"backgrounds/skyline.png")
+        self.rect = self.image.get_rect()
+
+        self.x = x
+        self.y = y
+
+        self.rect.bottomleft = (x, y)
+
+
 tow = Obstacle(767, WINDOW_HEIGHT, "twin_towers")
 tow1 = Obstacle(1400, WINDOW_HEIGHT, "twin_towers")
 tow3 = Obstacle(1300, 200, "spy_balloon")
+stage = Background(0, 500)
+
 level_1_group = pygame.sprite.Group()
 level_1_group.add(tow)
 level_1_group.add(tow1)
 level_1_group.add(tow3)
 
 
+stage_group = pygame.sprite.GroupSingle()
+stage_group.add(stage)
 
 while running:
     # poll for events
@@ -68,10 +92,15 @@ while running:
     display_surface.fill("lightblue")
     display_surface.blit(player, player_rect)
 
+    #stage_surface.fill("white")
+    #stage_surface.blit(f"backgrounds/skyline.png")
 
     level_1_group.update()
     level_1_group.draw(display_surface)
     
+    #stage_group.update()
+    #stage_group.draw(stage_surface)
+
     player_move(player_rect)
 
     # display_surface.blit(tow, towrect)
